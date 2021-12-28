79d2db20d0e6caadac04d101ae9dc559bd44be49
Sam Harwell <sam@tunnelvisionlabs.com>
2021-12-28 15:25:24 -0800

    Add source generator project

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Runtime.InteropServices;
---
+++ test/TunnelVisionLabs.LanguageTypes.SourceGenerator.UnitTests/Verifiers/CSharpSourceGeneratorVerifier`1+Test.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace TunnelVisionLabs.LanguageTypes.SourceGenerator.UnitTests.Verifiers
+{
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Testing;
+    using Microsoft.CodeAnalysis.Testing;
+    using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+    public static partial class CSharpSourceGeneratorVerifier<TSourceGenerator>
+        where TSourceGenerator : IIncrementalGenerator, new()
+    {
+        public class Test : CSharpSourceGeneratorTest<EmptySourceGeneratorProvider, XUnitVerifier>
+        {
+            public Test()
+            {
+            }
+
+            public LanguageVersion LanguageVersion { get; set; } = LanguageVersion.Default;
+
+            protected override IEnumerable<ISourceGenerator> GetSourceGenerators()
+            {
+                yield return new TSourceGenerator().AsSourceGenerator();
+            }
+
+            protected override CompilationOptions CreateCompilationOptions()
+            {
+                var compilationOptions = base.CreateCompilationOptions();
+                return compilationOptions.WithSpecificDiagnosticOptions(
+                    compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+            }
+
+            protected override ParseOptions CreateParseOptions()
+            {
+                return ((CSharpParseOptions)base.CreateParseOptions()).WithLanguageVersion(LanguageVersion);
+            }
+        }
+    }
+}
