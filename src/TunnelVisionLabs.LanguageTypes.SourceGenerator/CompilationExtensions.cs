96a1bba1108af3fedc98b6bad497a5429955b81c
Sam Harwell <sam@tunnelvisionlabs.com>
2021-12-28 18:42:30 -0800

    Implement NullableSourceGenerator

---
+++ src/TunnelVisionLabs.LanguageTypes.SourceGenerator/CompilationExtensions.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace TunnelVisionLabs.LanguageTypes.SourceGenerator
+{
+    using Microsoft.CodeAnalysis;
+
+    internal static class CompilationExtensions
+    {
+        /// <summary>
+        /// Gets a type by its metadata name to use for code analysis within a <see cref="Compilation"/>. This method
+        /// attempts to find the "best" symbol to use for code analysis, which is the symbol matching the first of the
+        /// following rules.
+        ///
+        /// <list type="number">
+        ///   <item><description>
+        ///     If only one type with the given name is found within the compilation and its referenced assemblies, that
+        ///     type is returned regardless of accessibility.
+        ///   </description></item>
+        ///   <item><description>
+        ///     If the current <paramref name="compilation"/> defines the symbol, that symbol is returned.
+        ///   </description></item>
+        ///   <item><description>
+        ///     If exactly one referenced assembly defines the symbol in a manner that makes it visible to the current
+        ///     <paramref name="compilation"/>, that symbol is returned.
+        ///   </description></item>
+        ///   <item><description>
+        ///     Otherwise, this method returns <see langword="null"/>.
+        ///   </description></item>
+        /// </list>
+        /// </summary>
+        /// <param name="compilation">The <see cref="Compilation"/> to consider for analysis.</param>
+        /// <param name="fullyQualifiedMetadataName">The fully-qualified metadata type name to find.</param>
+        /// <param name="requiresAccess"><see langword="true"/> to only consider accessible types; otherwise, <see langword="false"/> to consider all types.</param>
+        /// <returns>The symbol to use for code analysis; otherwise, <see langword="null"/>.</returns>
+        public static INamedTypeSymbol? GetBestTypeByMetadataName(this Compilation compilation, string fullyQualifiedMetadataName, bool requiresAccess)
+        {
+            // Try to get the unique type with this name, ignoring accessibility
+            var type = compilation.GetTypeByMetadataName(fullyQualifiedMetadataName);
+
+            // Otherwise, try to get the unique type with this name originally defined in 'compilation'
+            type ??= compilation.Assembly.GetTypeByMetadataName(fullyQualifiedMetadataName);
+
+            // Otherwise, try to get the unique accessible type with this name from a reference
+            if (type is null)
+            {
+                foreach (var module in compilation.Assembly.Modules)
+                {
+                    foreach (var referencedAssembly in module.ReferencedAssemblySymbols)
+                    {
+                        var currentType = referencedAssembly.GetTypeByMetadataName(fullyQualifiedMetadataName);
+                        if (currentType is null)
+                        {
+                            continue;
+                        }
+
+                        switch (currentType.GetResultantVisibility())
+                        {
+                            case SymbolVisibility.Public:
+                            case SymbolVisibility.Internal when referencedAssembly.GivesAccessTo(compilation.Assembly):
+                                break;
+
+                            default:
+                                continue;
+                        }
+
+                        if (type is object)
+                        {
+                            // Multiple visible types with the same metadata name are present
+                            return null;
+                        }
+
+                        type = currentType;
+                    }
+                }
+            }
+
+            if (requiresAccess && type is { DeclaredAccessibility: Accessibility.Internal } && !type.ContainingAssembly.GivesAccessTo(compilation.Assembly))
+            {
+                return null;
+            }
+
+            return type;
+        }
+    }
+}
---
+++ src/TunnelVisionLabs.LanguageTypes.SourceGenerator/SymbolVisibility.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace TunnelVisionLabs.LanguageTypes.SourceGenerator
+{
+    internal enum SymbolVisibility
+    {
+        Public,
+        Internal,
+        Private,
+    }
+}
